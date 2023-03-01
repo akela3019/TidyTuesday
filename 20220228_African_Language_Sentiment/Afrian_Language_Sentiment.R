612bb8ee147ee1928ec37457061169ab6f9032c6
akela3019 <akela3019@gmail.com>
2023-02-28 22:02:12 -0500

    Minor adjustments

--- 20220228_African_Language_Sentiment/Afrian_Language_Sentiment.R
+++ 20220228_African_Language_Sentiment/Afrian_Language_Sentiment.R
@@ -12,10 +12,11 @@ theme_custom <- theme(
   plot.title = element_text(hjust = 0.5, size = 10.5, face = "bold"),
   legend.title = element_text(size = 10.5, face = "bold"),
   strip.clip = "off",
-  axis.text.x = element_text(angle = 40, hjust = 1, size = 9),
   panel.spacing.y = unit(1.1, "mm"),
   panel.border = element_rect(color = "#ababab", fill = NA, linewidth = 0.3),
-  axis.line = element_blank()
+  axis.line = element_blank(),
+  axis.text.x = element_text(angle = 40, hjust = 1, size = 9, color = "#373634"),
+  axis.text.y = element_text(size = 9, color = "#373634")
 )
 
 # Load Data --------------------------
@@ -82,7 +83,8 @@ p_language <- afrisenti_summ %>%
   guides(fill = guide_legend(keywidth = 0.8, keyheight = 0.8)) +
   labs(x = NULL, y = NULL) +
   theme_custom +
-  theme(axis.text.y = element_blank(),
+  theme(plot.background = element_rect(fill = "#ecf0f1", color = NA),
+        axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
         strip.text.y.left = element_blank(),
         legend.position = "top",
@@ -101,8 +103,9 @@ p_region <- languages_summ %>%
   scale_fill_manual(values = fresh_palette) +
   labs(x = NULL, y = NULL, title = toupper("Countries spoken in")) +
   theme_custom + 
-  theme(strip.text = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1, size = 9),
+  theme(plot.background = element_rect(fill = "#ecf0f1", color = NA),
+        strip.text = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 9, color = "#373634"),
         axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
         legend.position = "none",
@@ -116,26 +119,27 @@ p_n_tweets <- afrisenti %>%
   left_join(languages, by = "language_iso_code") %>%
   mutate(language = factor(language, levels(languages$language))) %>%
   ggplot(aes(y = language, x = n)) +
-  geom_col(width = 0.85, fill = "#353525") +
+  geom_col(width = 0.85, fill = "#373634") +
   facet_grid(str_replace(language_family, "\\-", "-\n") %>% str_replace(" ", "\n") ~ ., 
              scale = "free_y", space = "free_y", switch = "y") +
-  scale_x_reverse(labels = function(x) {
-                  x = paste0(x/1E3, "K"); x[x == '0K'] <- 0; x })+
+  scale_x_reverse(labels = function(x) {x = paste0(x/1E3, "K"); x[x == '0K'] <- 0; x },
+                  breaks = seq(0, 2E4, 1E4))+
   scale_y_discrete(limits = rev, labels = function(x) {str_replace(x, " ", "\n")})+
   labs(x = NULL, y = NULL, title = toupper("Language    Family  No. of tweets"),
        caption = "@akela@mstdn.social") +
   coord_cartesian(expand = FALSE, clip = "off") +
   theme_custom + 
-  theme(plot.title = element_text(hjust = 1.12),
-        plot.caption = element_text(size = 10.5, hjust = 4.5, vjust = 1,
+  theme(plot.background = element_rect(fill = "#ecf0f1", color = NA),
+        plot.title = element_text(hjust = 0.95),
+        plot.caption = element_text(size = 10.5, hjust = 3, vjust = 1,
                                     face = "bold", margin = margin(t = 5, l = 0)),
         panel.border = element_rect(color = "#ababab", fill = NA, linewidth = 0.3),
-        strip.text.y.left = element_text(angle = 0, margin = margin(l = 2, r = 2), size = 9),
+        strip.text.y.left = element_text(angle = 0, margin = margin(l = 1, r = 1), size = 8.5, color = "#373634"),
         strip.background = element_rect(fill = "#ddd8d3", color = "#ababab", linewidth = 0.3),
         panel.grid.major.x = element_line(color = "#ababab", linewidth = 0.4, linetype = 3),
         panel.grid.major.y = element_blank(),
         plot.margin = margin(5, 5, 5, 10),
-        axis.text.y = element_text(face = "bold")
+        axis.text.y = element_text(size = 9.5)
         )
 
 
@@ -168,11 +172,13 @@ data_source <- paste(
     'arXiv preprint arXiv:2302.08956 (2023).'), 
   collapse = "<br/>")
 data_descript <- paste0(
-  "Sentiment analysis dataset covering 110,000+ annotated\n",
-  "tweets in 14 African languages")
+  "Sentiment analysis dataset covering 110,000+\n",
+  "annotated tweets in 14 African languages")
 
 p_map <- afr_tidy %>%
   ggplot(aes(x = long, y = lat, fill = region)) + 
+  geom_rect(data = NULL, aes(xmin = min(long), xmax = max(long),
+                ymin = -50, ymax = 45), fill = "#ddd8d3") +
   geom_polygon(aes(group = group), color = "#ababab", linewidth = 0.3, fill = "#f6f1eb") +
   geom_polygon(data = subset(afr_tidy, !is.na(region)), aes(group = group), 
                color = "#373634", linewidth = 0.4) +
@@ -182,13 +188,13 @@ p_map <- afr_tidy %>%
                    box.padding = 0.45, hjust = 0.5, vjust = 0.5, max.overlaps = Inf,
                    label.padding = 0.18, family = "Corbel", size = 3, show.legend = FALSE,
                    label.size = 0.3, segment.size = 0.3) +
-  geom_text(data = data.frame(x = -25, y = 47.5, label = data_descript), 
+  geom_text(data = data.frame(x = -25, y = 54, label = data_descript), 
             aes(x = x, y = y, label = label), vjust = 1, hjust = 0, size = 4, 
             family = "Corbel", color = "#373634", 
             lineheight = 1, inherit.aes = FALSE) +
   scale_color_identity() +
   scale_fill_manual(values = fresh_palette, na.translate = FALSE, name = "REGION") +
-  scale_y_continuous(limits = c(-50, 48)) +
+  scale_y_continuous(limits = c(-50, 55)) +
   guides(fill = guide_legend(keywidth = 0.8, keyheight = 0.8)) +
   labs(title = "AfriSenti", caption = data_source) +
   coord_cartesian(expand = FALSE, clip = "off") +
@@ -203,18 +209,18 @@ p_map <- afr_tidy %>%
         panel.background = element_blank(),
         panel.border = element_blank(),
         legend.background = element_blank(), 
-        plot.background = element_rect(fill = "#373634"),
+        plot.background = element_rect(fill = NA),
         plot.title = element_text(size = 18, hjust = 0, margin = margin(b = 0), 
                                   face = "bold", family = "Corbel"),
-        plot.caption = element_markdown(size = 10, hjust = 0, vjust = 1, 
-                                        lineheight = 1.1, margin = margin(t = -3)))
+        plot.caption = element_markdown(size = 9.5, hjust = 0, vjust = 1, 
+                                        lineheight = 1.1, margin = margin(t = 0)))
 
 
 
 ## Arrange panels ----------------------------
 g <- egg::ggarrange(p_n_tweets, p_language, p_region, p_map, nrow = 1, 
-                    widths = c(0.25, 1, 0.65, 0.8))
+                    widths = c(0.25, 0.9, 0.7, 0.88))
 ggsave("20220228_African_Language_Sentiment/african_language.png",
-       g, height = 6, width = 15, dpi = 600)
+       g, height = 6, width = 13, dpi = 600)
 
 
